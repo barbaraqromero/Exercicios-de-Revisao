15735e0f7179aebb34f382407d062abca9ec081e
Bárbara Romero <bqromero@gmail.com>
2021-08-31 17:15:31 -0300

    Exercicio 1 realizado com sucesso

---
+++ br/com/zup/Exercicio1.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Exercicio1 {
+
+  public static void main(String[] args) {
+    int contador = 1;
+
+    while(contador >=1 & contador <= 100){
+      System.out.println(contador);
+      contador++;
+    }
+  }
+}
---
+++ br/com/zup/Exercicio1For.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+public class Exercicio1For {
+  public static void main(String[] args) {
+    for (int i= 1; i >=1 & i<=100; i++)
+      System.out.println(i);
+  }
+}
