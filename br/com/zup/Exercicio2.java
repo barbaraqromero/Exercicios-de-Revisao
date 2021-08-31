347f5f00f99abf917d824656b64122db2b20038d
Bárbara Romero <bqromero@gmail.com>
2021-08-31 17:48:25 -0300

    Exercicio 2 concluido com sucesso

--- br/com/zup/Exercicio2.java
+++ br/com/zup/Exercicio2.java
@@ -1,2 +1,30 @@
-package br.com.zup;public class Exercicio2 {
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Por favor, digite a quantidade de números a serem analisados:");
+    int numeroLido = leitor.nextInt();
+
+    int contador = 1;
+    int numeroEscolhido = 0;
+    int soma = 0;
+    int media = 0;
+
+
+    while (contador <= numeroLido){
+      System.out.println("Digite o número escolhido: ");
+      numeroEscolhido = leitor.nextInt();
+      contador++;
+      soma = soma+numeroEscolhido;
+      media = soma/numeroLido;
+
+
+    }
+    System.out.println(soma);
+    System.out.println("A média da soma dos números escolhidos é: "  + media);
+  }
 }
