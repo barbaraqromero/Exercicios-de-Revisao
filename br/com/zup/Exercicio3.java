fafcba6f8a22fbf630c979e0ae1154a8cfc19f41
Bárbara Romero <bqromero@gmail.com>
2021-08-31 19:03:40 -0300

    Exercicio 3 concluido com sucesso

--- br/com/zup/Exercicio3.java
+++ br/com/zup/Exercicio3.java
@@ -1,2 +1,38 @@
-package br.com.zup;public class Exercicio3 {
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    int totalDeNumeros = 10;
+    int menorValor = 0;
+    int maiorValor = 0;
+    int contador = 2;
+
+    System.out.println("Escolha 10 números aleatórios");
+    System.out.println("Insira o 1º número: ");
+    int valorEscolhido = leitor.nextInt();
+    menorValor = valorEscolhido;
+    maiorValor = valorEscolhido;
+
+    while (contador <= totalDeNumeros) {
+      System.out.println("Insira o " + contador + "º número: ");
+      valorEscolhido = leitor.nextInt();
+      contador++;
+      if (valorEscolhido < menorValor){
+        menorValor = valorEscolhido;
+      }
+      if (valorEscolhido > maiorValor){
+        maiorValor = valorEscolhido;
+      }
+
+
+
+    }
+    System.out.println("O menor número é: " + menorValor);
+    System.out.println("O maior número é: " + maiorValor);
+  }
+
 }
+
