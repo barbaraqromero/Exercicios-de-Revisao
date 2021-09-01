576c78d1e4759d774f393e0b48be9c3ab13f9d1a
Bárbara Romero <bqromero@gmail.com>
2021-08-31 21:03:09 -0300

    Exercicio 4 concluido com sucesso

--- br/com/zup/Exercicio4.java
+++ br/com/zup/Exercicio4.java
@@ -1,4 +1,24 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de números desejada para análise:");
+    int numeroLido = leitor.nextInt();
+
+    int contador = 1;
+    int soma = 0;
+
+    while (contador <= numeroLido){
+      System.out.println("Digite o " + contador + "º número");
+      int numeroEscolhido = leitor.nextInt();
+      soma = soma + numeroEscolhido;
+      contador++;
+
+    }
+    System.out.println("A soma dos números escolhidos é " + soma);
+  }
 }
