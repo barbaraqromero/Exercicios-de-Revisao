7eb41261ca03cd4a001c0685a575e3bee2a1f40a
Bárbara Romero <bqromero@gmail.com>
2021-08-31 21:27:31 -0300

    Exercicio 5 concluido com sucesso

--- br/com/zup/Exercicio5.java
+++ br/com/zup/Exercicio5.java
@@ -2,6 +2,11 @@ package br.com.zup;
 
 public class Exercicio5 {
   public static void main(String[] args) {
+    int contador = 0;
 
+    while (contador <= 100000) {
+      System.out.println(contador);
+      contador = contador + 1000;
+    }
   }
 }
